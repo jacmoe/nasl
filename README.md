69cae7e2e94fd65c1f6a359a70b886dcb1265316
Jacob Moen <jacmoe.dk@gmail.com>
2017-05-03 14:58:43 +0200

    Boilerplate stuff

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
+
+    /*
+    *   This file is part of the
+    * ███╗   ██╗ █████╗ ███████╗██╗     
+    * ████╗  ██║██╔══██╗██╔════╝██║     
+    * ██╔██╗ ██║███████║███████╗██║     
+    * ██║╚██╗██║██╔══██║╚════██║██║     
+    * ██║ ╚████║██║  ██║███████║███████╗
+    * ╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝╚══════╝
+    *   project : https://github.com/jacmoe/nasl
+    *
+    *   Copyright 2017 Jacob Moen
+    *
+    */
+
 # nasl
 Nasl is Not A Sixteen-bit Library
---
+++ src/main.c
@@ -0,0 +1,22 @@
+/*
+*   This file is part of the
+* ███╗   ██╗ █████╗ ███████╗██╗     
+* ████╗  ██║██╔══██╗██╔════╝██║     
+* ██╔██╗ ██║███████║███████╗██║     
+* ██║╚██╗██║██╔══██║╚════██║██║     
+* ██║ ╚████║██║  ██║███████║███████╗
+* ╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝╚══════╝
+*   project : https://github.com/jacmoe/nasl
+*
+*   Copyright 2017 Jacob Moen
+*
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+void main()
+{
+    printf("Hello, NASL NASL!\n");
+}
