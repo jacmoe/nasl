f8fd324fa082496bf297c9a6861aaec2d164cf7f
Jacob Moen <jacmoe.dk@gmail.com>
2017-05-05 00:56:28 +0200

    Conan does indeed update package when needed

--- test_package/main.c
+++ test_package/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 #include "nasl_script.h"
+#include "nasl_color.h"
 
 int main()
 {
     nasl_script_init();
-    printf("hey man\n");
-    printf("%#08x\n", GREY3);
+    printf("Printing colors:\n");
+    printf("YELLOW : %#08x\n", YELLOW);
+    printf("c64_palette[7] : %#08x\n", c64_palette[7]);
     nasl_script_shutdown();
 }
