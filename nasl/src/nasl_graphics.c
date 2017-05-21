0226f07a93d021277bf86f8fab3019386391849a
Jacob Moen <jacmoe.dk@gmail.com>
2017-05-21 15:28:55 +0200

    SDL renders .. (reverted from commit 0f38495dbeca07b83b0fafb5cbbb7d312de2324c)

--- nasl/src/nasl_graphics.c
+++ nasl/src/nasl_graphics.c
@@ -22,30 +22,6 @@ static SDL_GLContext glcontext;
 // Flags
 static int resizef;
 
-static const char *vertexSource = "#version 330 core\n"
-"\n"
-"in vec2 position;\n"
-"in vec2 texcoord;\n"
-"\n"
-"out vec3 Color;\n"
-"out vec2 Texcoord;\n"
-"\n"
-"void main() {\n"
-"    Texcoord = texcoord;\n"
-"    gl_Position = vec4(position, 0.0, 1.0);\n"
-"}";
-
-static const char *fragmentSource = "#version 330 core\n"
-"\n"
-"in vec2 Texcoord;\n"
-"\n"
-"out vec4 outColor;\n"
-"\n"
-"uniform sampler2D tex;\n"
-"\n"
-"void main() {\n"
-"    outColor = texture(tex, Texcoord);\n"
-"}";
 
 static void error_callback(int e, const char *d)
 {
@@ -63,6 +39,113 @@ SDL_Window* nasl_graphics_get_window()
     return window;
 }
 
+static char *ReadFile(const char *path) {
+    FILE *f = fopen(path, "r");
+    if (!f) {
+        fprintf(stderr, "Coudn't read file: '%s'\n", path);
+    }
+
+    fseek(f, 0, SEEK_END);
+    size_t fsize = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    char *buf = malloc(fsize + 1);
+    fread(buf, fsize, 1, f);
+    buf[fsize] = '\0';
+
+    fclose(f);
+
+    return buf;
+}
+
+
+static GLuint CompileShaderProgram() {
+    char *vertexSource = ReadFile("assets/shaders/vertex.glsl");
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, (const GLchar **)&vertexSource, NULL);
+    glCompileShader(vertexShader);
+    free(vertexSource);
+
+    char *fragmentSource = ReadFile("assets/shaders/frag.glsl");
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, (const GLchar **)&fragmentSource, NULL);
+    glCompileShader(fragmentShader);
+    free(fragmentSource);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
+
+
+static GLuint BuildQuad(GLuint shaderProgram) {
+    GLfloat vertices[] = {
+        // Vertex coord  Texture coord
+        1,  1,           1, 0,
+        -1,  1,          0, 0,
+        1, -1,           1, 1,
+        -1, -1,          0, 1
+    };
+
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Vertex attributes
+    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float),
+            0);
+    glEnableVertexAttribArray(posAttrib);
+
+    GLint texAttrib = glGetAttribLocation(shaderProgram, "texcoord");
+    glVertexAttribPointer(texAttrib, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float),
+            (void *)(2 * sizeof(float)));
+    glEnableVertexAttribArray(texAttrib);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    return vao;
+}
+
+
+static GLuint BuildTexture(int width, int height) {
+    GLuint tex;
+
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA,
+            GL_UNSIGNED_BYTE, NULL);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    return tex;
+}
+
+
+static void InitOpenGL(int width, int height) {
+    GLuint shaderProgram = CompileShaderProgram();
+    GLuint quad_vao = BuildQuad(shaderProgram);
+    GLuint tex = BuildTexture(width, height);
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(quad_vao);
+    glBindTexture(GL_TEXTURE_2D, tex);
+}
 
 int HandleResize(void *userdata, SDL_Event *ev) {
     if (ev->type == SDL_WINDOWEVENT) {
@@ -89,7 +172,7 @@ int nasl_graphics_init(int width, int height, const char* title, int fullscreen,
     window = SDL_CreateWindow(
             title,
             SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-            width * scalefactor, height * scalefactor,
+            width, height,
             SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL
             );
 
@@ -104,128 +187,39 @@ int nasl_graphics_init(int width, int height, const char* title, int fullscreen,
     // Init GLEW
     glewExperimental = GL_TRUE;
     glewInit();
-
-    // Quad vertices ...
-    float vertices[] = {
-        // Position   Texcoords
-        -1,  1,   0, 0,  // Top-left
-        1,  1,   1, 0,  // Top-right
-        1, -1,   1, 1,  // Bottom-right
-        -1, -1,   0, 1,  // Bottom-left
-    };
-
-    // ... elements ...
-    GLuint elements[] = {
-        0, 1, 2,
-        2, 3, 0,
-    };
-
-    // ... and texture
-    GLuint tex;
-    glGenTextures(1, &tex);
-    glBindTexture(GL_TEXTURE_2D, tex);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA,
-            GL_UNSIGNED_BYTE, NULL);
-
-    // Vertex Array Object
-    GLuint vao;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-
-    // Vertex Buffer Object
-    GLuint vbo;
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // Element Buffer Object
-    GLuint ebo;
-    glGenBuffers(1, &ebo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements,
-            GL_STATIC_DRAW);
-
-    // Shaders
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexSource, NULL);
-    glCompileShader(vertexShader);
-
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
-    glCompileShader(fragmentShader);
-
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glUseProgram(shaderProgram);
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    // Vertex attributes
-    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
-    glEnableVertexAttribArray(posAttrib);
-    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE,
-            4 * sizeof(float), 0);
-
-    GLint texAttrib = glGetAttribLocation(shaderProgram, "texcoord");
-    glEnableVertexAttribArray(texAttrib);
-    glVertexAttribPointer(texAttrib, 2, GL_FLOAT, GL_FALSE,
-            4 * sizeof(float), (void *)(2 * sizeof(float)));
-
-    // Clear with black
-    glClearColor(0, 0, 0, 1);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    return 0;
+    InitOpenGL(width, height);
+   return 0;
 }
 
 int nasl_graphics_shutdown()
 {
-    SDL_GL_DeleteContext(glcontext);
-    IMG_Quit();
-    SDL_Quit();
+    glfwTerminate();
     return 0;
 }
 
 int nasl_graphics_running()
 {
-    return 1;//!glfwWindowShouldClose(window);
+    return !glfwWindowShouldClose(window);
 }
 
 static uint32_t get_time()
 {
-    return SDL_GetTicks();
+    return 1000 * glfwGetTime();
 }
 
 uint32_t nasl_graphics_render(Buffer *buf)
 {
     uint32_t start = get_time();
 
-    if (resizef) {
-        resizef = 0;
-
-        int winwidth, winheight;
-        SDL_GetWindowSize(window, &winwidth, &winheight);
-
-        glViewport(0, 0, winwidth, winheight);
-    }
-
     glTexSubImage2D(
-            GL_TEXTURE_2D,
-            0, 0,
-            0,
-            buf->width, buf->height,
-            GL_RGBA, GL_UNSIGNED_BYTE,
-            buf->pixels
+            GL_TEXTURE_2D,              // target
+            0,                          // level of detail
+            0, 0,                       // xoffset, yoffset
+            buf->width, buf->height,    // width, height
+            GL_RGBA, GL_UNSIGNED_BYTE,  // format, type
+            buf->pixels                 // pixels
             );
-
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
     return get_time() - start;
 }
@@ -233,10 +227,10 @@ uint32_t nasl_graphics_render(Buffer *buf)
 
 void nasl_graphics_present()
 {
-    SDL_GL_SwapWindow(window);
+    glfwSwapBuffers(window);
 }
 
 void nasl_graphics_poll_events()
 {
-    //glfwPollEvents();
+    glfwPollEvents();
 }
