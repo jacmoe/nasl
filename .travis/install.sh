56d67fdb05009b2aab0f1c780b7690e1805deff8
Jacob Moen <jacmoe.dk@gmail.com>
2017-06-12 14:28:05 +0200

    Update install.sh

--- .travis/install.sh
+++ .travis/install.sh
@@ -22,7 +22,6 @@ fi
 pip install conan_package_tools # It install conan too
 pip install conan --upgrade
 conan remote list
-conan remote remove conan-center
 conan remote add dimi https://api.bintray.com/conan/dimi309/conan-packages
 conan remote modify dimi https://api.bintray.com/conan/dimi309/conan-packages
 conan remote list
