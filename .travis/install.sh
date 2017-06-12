081c0ed4c7a941462fc0afa2430e8b3a8ea06427
Jacob Moen <jacmoe.dk@gmail.com>
2017-06-12 04:44:03 +0200

    sadfsadf

--- .travis/install.sh
+++ .travis/install.sh
@@ -20,5 +20,5 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
 fi
 
 pip install conan_package_tools # It install conan too
-conan user
 conan remote add dimi309-bintray https://api.bintray.com/conan/dimi309/conan-packages
+conan user
